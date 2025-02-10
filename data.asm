20837c0feddd69e613512008ef1c2723df76389a
Welby Seely <welbyseely@gmail.com>
2025-02-09 21:37:53 -0500

    Removed copy-pasted lines from data.asm

--- data.asm
+++ data.asm
@@ -1,35 +1,4 @@
 bits 16
-BIOS_VIDEO          equ 0x10
-DISPLAY_FUN         equ 0x13
-
-FUN_VIDEO_MODE      equ 0x0000
-VGA_MODE            equ 0x0013
-
-VGA_DISPLAY_WIDTH   equ 320
-DISPLAY_WIDTH       equ 80
-DISPLAY_HEIGHT      equ 25
-VGA_TXT_DISP_WIDTH  equ 40
-VGA_TXT_DISP_HEIGHT equ 25
-MESSAGE_ROW         equ VGA_TXT_DISP_HEIGHT / 2 + 3
-LINE_ROW_TOP        equ MESSAGE_ROW - 1
-LINE_ROW_NAME       equ MESSAGE_ROW + 1
-LINE_ROW_BOTTOM     equ LINE_ROW_NAME + 1
-LINE_ROW_ANYKEY     equ LINE_ROW_BOTTOM + 2
-TEXT_MODE           equ 0x03
-MAGENTA_BLACK       equ 0x0D
-WHITE_BLACK         equ 0x0F
-RED_BLACK           equ 0x04
-YELLOW_BLACK        equ 0x0E
-LIGHT_RED           equ 0x0C
-LOGO_START_X        equ (VGA_DISPLAY_WIDTH - (16 * SCALING_FACTOR)) / 2
-LOGO_START_Y        equ (200 - (9 * SCALING_FACTOR)) / 2 -40
-
-SCALING_FACTOR      equ 0x8
-FALSE               equ 0x00
-
-
-%define CENTER_TXT(len) ((DISPLAY_WIDTH - len) / 2)
-%define CENTER_VGA_TXT(len) ((VGA_TXT_DISP_WIDTH - len) / 2)
 
 org 0x7e00
 topline             db 0xC9
@@ -57,8 +26,3 @@ w_bitmap db 02h, 80h
 red_shades db 58, 55, 50, 45, 40, 35, 30, 25, 20; Bright to dark red
 ; Row color table, from top to bottom row
 row_colors db 32, 33, 34, 35, 36, 37, 38, 39, 40  ; Use only custom red shades
-; Pad to 512 bytes for an MBR:
-padding times 510 - ($ - $$) db 0
-
-; Optional boot signature:
-bootSig db 0x55, 0xAA
