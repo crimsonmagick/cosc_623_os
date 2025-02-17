fd9e13cd26f8c167a5118dd428ceb7644ab23bd7
Welby Seely <welbyseely@gmail.com>
2025-02-16 21:36:03 -0500

    Added datetime file

--- Makefile
+++ Makefile
@@ -14,6 +14,8 @@ MBR_BIN		= $(subst .asm,.bin,$(MBR_SRC))
 DATA_SRC	=  loaderV02.asm
 DATA_BIN	=  data.bin
 
+DATE_SRC	=  datetimeV03.asm
+DATE_BIN	=  date.bin
 
 .PHONY : everything
 
@@ -21,20 +23,24 @@ DATA_BIN	=  data.bin
 
 all: everything
 
-everything : $(MBR_BIN) $(DATA_BIN)
+everything : $(MBR_BIN) $(DATA_BIN) $(DATE_BIN)
  ifneq ($(wildcard $(IMG)), )
  else
 		dd if=/dev/zero of=$(IMG) bs=512 count=2880
  endif
 
 		dd if=$(MBR_BIN) of=$(IMG) bs=512 count=1 conv=notrunc
-		dd if=$(DATA_BIN) of=$(IMG) bs=512 seek=37 conv=notrunc
+		dd if=$(DATA_BIN) of=$(IMG) bs=512 count=1 seek=37 conv=notrunc
+		dd if=$(DATE_BIN) of=$(IMG) bs=512 count=1 seek=40 conv=notrunc
 
 $(MBR_BIN) : $(MBR_SRC)
 #	nasm -f bin $< -o $@
 	$(ASM) $(ASMFLAGS) $< -o $@
 
 $(DATA_BIN) : $(DATA_SRC)
+	$(ASM) $(ASMFLAGS) $< -o $@
+
+$(DATE_BIN) : $(DATE_SRC)
 	$(ASM) $(ASMFLAGS) $< -o $@
 
 clean :
---
+++ datetimeV03.asm
@@ -0,0 +1,59 @@
+; 8/11/2010
+
+	;bit16			;16bit by default
+	org 0x100		;use as .com
+
+%macro makedt 4
+	mov bh,%1 			;dh/dl/chcl
+	shr bh,4
+	add bh,30h 			;add 30h to convert to ascii
+	mov [%2 + %3],bh
+	mov bh,%1
+	and bh,0fh
+	add bh,30h
+	mov [%2 + %4],bh
+%endmacro
+
+%macro	dsp 4
+	mov ah,13h		;Function 13h (Display String)
+	mov al,0		;Write mode is zero
+	mov bh,0		;Use video page of zero
+	mov bl,0ah		;Attribute (lightgreen on black)
+	mov cx,%1		;Characters in string
+	mov dh,%2		;Position on row 1
+	mov dl,%3		;and column
+	lea bp,%4		;Load the offset address of string
+	int 10h
+%endmacro
+
+	mov ah,04h	 ;function 04h (get RTC date)
+	int 1Ah		;BIOS Interrupt 1Ah (Read Real Time Clock)
+
+;CH - Century
+;CL - Year
+;DH - Month
+;DL - Day
+
+	makedt dh, dtfld, 0, 1
+	makedt dl, dtfld, 3, 4
+	makedt ch, dtfld, 6, 7
+	makedt cl, dtfld, 8, 9
+	dsp 10, 1, 0, [dtfld]
+
+	mov ah,02h
+	int 1Ah
+
+;CH - Hours
+;CL - Minutes
+;DH - Seconds
+
+	makedt ch, tmfld, 0, 1
+	makedt cl, tmfld, 3, 4
+	makedt dh, tmfld, 6, 7
+	dsp 8, 2, 0, [tmfld]
+
+
+dtfld: db '00/00/0000'
+tmfld: db '00:00:00'
+
+	int 20h
